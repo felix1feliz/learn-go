e620917829264ae0ffd95b1b1e225d034ca32f94
felix_feliz <sonicrobogigante@gmail.com>
2024-08-09 18:51:41 -0300

    Get url queries

--- main.go
+++ main.go
@@ -4,16 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 func main() {
 	const PORT = ":3000"
 
-	http.HandleFunc("GET /", func(res http.ResponseWriter, req *http.Request) {
-		var cookie http.Cookie
-		cookie.Name = "Tom"
-		cookie.Value = "IsAGenius"
-		setCookie(res, cookie)
+	http.HandleFunc("GET /{$}", func(res http.ResponseWriter, req *http.Request) {
+		var urlQueryValues url.Values = req.URL.Query()
+		if urlQueryValues.Has("id") {
+			var cookie http.Cookie
+			cookie.Name = "id"
+			cookie.Value = urlQueryValues.Get("id")
+			setCookie(res, cookie)
+		}
 		res.WriteHeader(200)
 	})
 
@@ -21,7 +25,7 @@ func main() {
 }
 
 func setCookie(res http.ResponseWriter, c http.Cookie) {
-	var c_valid = c.Valid()
+	var c_valid error = c.Valid()
 	if c_valid != nil {
 		log.Fatal(c_valid)
 	}
@@ -30,7 +34,7 @@ func setCookie(res http.ResponseWriter, c http.Cookie) {
 
 func listen(PORT string) {
 	fmt.Printf("Server listening on port '%s'\n", PORT)
-	listen_err := http.ListenAndServe(PORT, nil)
+	var listen_err error = http.ListenAndServe(PORT, nil)
 	if listen_err != nil {
 		log.Fatal(listen_err)
 	}
